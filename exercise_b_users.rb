626f0b1c6311ff06ab62d206086d51374a933d6a
Chris Jennings <chrisjennings555@gmail.com>
2018-10-17 18:41:53 +0100

    second exercise complete

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -55,12 +55,57 @@ users = {
 }
 
 # 1. Get Jonathan's Twitter handle (i.e. the string `"jonnyt"`)
+p users["Jonathan"][:twitter]
+
 # 2. Get Erik's hometown
+p users["Erik"][:home_town]
+
 # 3. Get the array of Erik's lottery numbers
+p users["Erik"][:lottery_numbers]
+
 # 4. Get the type of Avril's pet Monty
+p users["Avril"][:pets]
+
 # 5. Get the smallest of Erik's lottery numbers
+p users["Erik"][:lottery_numbers].min
+
 # 6. Return an array of Avril's lottery numbers that are even
+even_numbers = users["Avril"][:lottery_numbers]
+even_numbers.each { |x| puts x if x.even? }
+
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
+
+users["Erik"][:lottery_numbers] = [18, 34, 8, 11, 24, 7]
+
+p users["Erik"][:lottery_numbers]
 # 8. Change Erik's hometown to Edinburgh
+users["Erik"][:home_town] = ["Edinburgh"]
+
+p users["Erik"][:home_town]
 # 9. Add a pet dog to Erik called "Fluffy"
+ users["Erik"][:pets] = [
+   {
+     :name => "nemo",
+     :species => "fish"
+   },
+   {
+     :name => "kevin",
+     :species => "fish"
+   },
+   {
+     :name => "spike",
+     :species => "dog"
+   },
+   {
+     :name => "rupert",
+     :species => "parrot"
+   },
+   {
+     :name => "fluffy",
+     :species => "dog"
+   }
+ ]
+
+ p users["Erik"][:pets]
 # 10. Add another person to the users hash
